eee6b0d3d6d0ce5232871727417693b20ad48f4e
Yakshitha <yakshithamuradachedau@gmail.com>
2024-11-16 11:44:09 +0530

    Authentication have been created

---
+++ Backend/index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const authRoute = require("./routes/auth");
+
+dotenv.config();
+const app = express();
+
+// MIDDLEWARES 
+app.use(cors());
+app.use(express.json());
+
+  //ROUTES
+   app.use("/auth",authROute)
+
+// DATABASE CONNECTION
+const DB = process.env.DB;
+mongoose.connect(DB).then(()=>{
+    console.log("DB connection is successful");
+}) .catch((err)=>{
+ console.log(err)
+})
+
+// SERVER 
+
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, () =>{
+    console.log(`Server is running on port ${PORT}`)
+})
+
+
+ 
